c166effe470f33a761e805073a3db9d1710346b6
bharats97 <bharatsardana347@gmail.com>
2020-11-26 05:07:30 +0530

    Adds signup

---
+++ signup/index.php
@@ -0,0 +1,9 @@
+<script type="text/javascript" src="js/validation.js"></script>
+<form action="dbinsert.php" method="post">
+    <input type="text" class="txt-fld" name="firstname" placeholder="First Name" onkeyup=validateFname()>
+    <input type="text" class="txt-fld" name="middlename" placeholder="Middle Name">
+    <input type="text" class="txt-fld" name="lastname" placeholder="Last Name">
+    <input type="text" class="txt-fld" name="contact" placeholder="Phone Number">
+    <input type="password" class="txt-fld" name="password"placeholder="Password">
+    <input type="submit" class="btn" value="Sign Up">
+</form>
