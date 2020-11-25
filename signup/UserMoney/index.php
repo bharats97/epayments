b41c5114c47e0520bc891631431f3c18e0255469
bharats97 <bharatsardana347@gmail.com>
2020-11-25 19:55:13 +0530

    Adds signup

---
+++ signup/UserMoney/index.php
@@ -0,0 +1,6 @@
+<form action="#">
+    <input type="text" class="txt-fld" placeholder="Bank Name">
+    <input type="text" class="txt-fld" placeholder="Account No.">
+    <input type="text" class="txt-fld" placeholder="IFSC code">
+    <input type="submit" class="btn" value="Add Bank Account">
+</form>
---
+++ signup/index.php
@@ -0,0 +1,8 @@
+<form action="dbinsert.php" method="post">
+    <input type="text" class="txt-fld" name="firstname" placeholder="First Name">
+    <input type="text" class="txt-fld" name="middlename" placeholder="Middle Name">
+    <input type="text" class="txt-fld" name="lastname" placeholder="Last Name">
+    <input type="text" class="txt-fld" name="contact" placeholder="Phone Number">
+    <input type="password" class="txt-fld" name="password"placeholder="Password">
+    <input type="submit" class="btn" value="Sign Up">
+</form>
