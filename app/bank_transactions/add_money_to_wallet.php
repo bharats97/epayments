5b1e4c21fccb1ad6fc2ab88c6563a68e69096e9d
Priyanka-Negi <pynkanegi@gmail.com>
2020-11-26 04:13:49 +0530

    updated add_money_to_wallet.php

--- app/bank_transactions/add_money_to_wallet.php
+++ app/bank_transactions/add_money_to_wallet.php
@@ -1,20 +1,18 @@
 <?php
 session_start();
-include("../connection/dbconnection_variables.php");
+include_once("../connection/connect.php");
 //dummy session variable user_id 
 
-function updateWalletBalance($user_id,$amount)
+function updateWalletBalance($user_id,$amount,$connection)
 {
-	$response_string=""
+	$response_string="";
 	$sql = "UPDATE `user_wallet` SET `wallet_balance`=`wallet_balance`+$amount WHERE `user_id`=$user_id ";
 	$response_string="";
 
-	if ($conn->query($sql) === TRUE) {
+	if ($connection->query($sql) === TRUE) {
 		$response_string="Rupees $amount/- added to your account <br>";
-		if($amount_deducted>0)
-			$response_string.="Rupees $amount_deducted/- service charge deducted ";
 	} else {
-  		$response_string= "Error adding money " . $conn->error;
+  		$response_string= "Error adding money " . $connection->error;
 	}
 	return $response_string;
 
@@ -31,40 +29,37 @@ $user_name="";
 if($card_type=="credit_card")
 {
 	$amount_deducted=$amount*0.02;
-	updateWalletBalance(0,$amount_deducted);
+	//add amount_deducted to admin's wallet
+	updateWalletBalance(0,$amount_deducted,$connection);
 	$amount=$amount-$amount_deducted;
 }
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
 // Update user wallet Balance
-updateWalletBalance($user_id,$amount)
-
+$response_string=updateWalletBalance($user_id,$amount,$connection);
+if($amount_deducted>0)
+	$response_string.="Rupees $amount_deducted/- service charge deducted ";
+	
 //Get UserName
-
 $sql="SELECT first_name,last_name FROM `user_details` WHERE user_id=$user_id";
-$result = $conn->query($sql);
+$result = $connection->query($sql);
 
 if ($result->num_rows > 0) {
   $row = $result->fetch_assoc();
   $user_name=$row["first_name"]." ".$row["last_name"];
 } 
 else {
-$response_string= "Error getting Username " . $conn->error;	
+$response_string= "Error getting Username " . $connection->error;	
 }
 
 // Update Transction Table
 $sql="INSERT INTO `transactions`(`receiver_id`, `sender_name`, `receiver_name`, `amount`, `comment`) VALUES ( '$user_id','Bank','$user_name',$amount,'You added money to your wallet from Bank')";
-if ($conn->query($sql) === FALSE) {
-  $response_string= "Error Updating transaction " . $conn->error;
+if ($connection->query($sql) === FALSE) {
+  $response_string= "Error Updating transaction " . $connection->error;
 }
 
 echo $response_string;
 
-$conn->close();
-
+include_once("../connection/disconnect.php");
 
 ?>
