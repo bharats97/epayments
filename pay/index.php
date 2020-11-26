875df41a8631c516781dec834296e11ad359d765
Priyanka-Negi <pynkanegi@gmail.com>
2020-11-26 17:57:06 +0530

    added form to pay money

---
+++ pay/index.php
@@ -0,0 +1,26 @@
+<?php
+include("../resources/header.php");
+?>
+<link rel="stylesheet" type="text/css" href="../resources/css/tab_style_form.css"> 
+
+<div class="forms">
+    <form action="#" id="pay_form">
+          <h1>Pay</h1>
+          <p id="error_message"></p>
+          <p id="response_message"></p>
+  
+          <div class="input-field">
+            <label for="pay_to">Mobile Number</label>
+            <input type="text" name="pay_to" id="pay_to" required/>
+            <label for="contact_name">Name </label>
+            <input type="text" name="contact_name" id="contact_name" required />
+            <label for="amount">Amount</label> 
+            <input type="text" name="amount" id="amount" required/>
+            <input type="submit" value="Pay" class="button" id="pay"/>
+          </div>
+      </form>
+
+</div>
+<?php
+include("../resources/footer.php");
+?>
