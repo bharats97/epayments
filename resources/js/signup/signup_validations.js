acaeea5e5174755469fe031a2e938d1a57bd03be
Ankush Khanna <ankushkhanna1998@gmail.com>
2020-11-28 03:01:56 +0530

    relocates code

--- resources/js/signup/signup_validations.js
+++ resources/js/signup/signup_validations.js
@@ -2,15 +2,23 @@ function validateFname() {
     var fname = document.getElementsByName("firstname");
     alert(isCharOnly(fname));
 }
+
 function validateMname() {
     var mname = document.getElementsByName("middlename");
     isCharOnly(mname);
 }
-function validateLFname() {
+
+function validateLname() {
     var lname = document.getElementsByName("lastname");
     isCharOnly(lname);
 }
+
 function isCharOnly()  {
     var char_rex = /^[A-Za-z]+$/;
     return name.value.match(char_rex);
+}
+
+function validatePassword() {
+    let pass = document.getElementByName('password');
+    return pass.length >= 8;
 }
--- signup/UserMoney/index.php
+++
@@ -1,6 +0,0 @@
-<form action="#">
-    <input type="text" class="txt-fld" placeholder="Bank Name">
-    <input type="text" class="txt-fld" placeholder="Account No.">
-    <input type="text" class="txt-fld" placeholder="IFSC code">
-    <input type="submit" class="btn" value="Add Bank Account">
-</form>
