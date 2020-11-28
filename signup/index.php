fe9086a7ef7832fa76911002cfb499319e452c87
Priyanka-Negi <pynkanegi@gmail.com>
2020-11-29 00:09:44 +0530

    Login/signup (#12)
    * updated login/signup page
    
    * designed user account page
    
    * added login/signup validations
    
    * Update resources/css/signup.css
    
    * removed unnecessary imports
    
    Co-authored-by: Ankush Khanna <ankushkhanna1998@gmail.com>

--- signup/index.php
+++ signup/index.php
@@ -3,6 +3,10 @@
 include("../resources/header.php");
 ?>
 <link rel="stylesheet" type="text/css" href="../resources/css/signup.css"> 
+
+<script src="../resources/js/auth/validate_contact.js"></script>
+<script src="../resources/js/signup/signup_validations.js"></script>
+
 <div class="float-container">
 	<div class="float-child">
 		<img src="../resources/images/transfer.jpg" height="100%" width="100%">
@@ -10,10 +14,27 @@ include("../resources/header.php");
 	<div class="float-child">
 
 		<div class="forms">
+
 			<form action="../app/signup/db_insert.php" method="post">
+
 				<h1>Sign Up</h1>
 				<p id="error_message"></p>
 				<p id="response_message"></p>
+
+			<form action="../app/signup/db_insert.php" method="post" id="signup_form">
+			
+				<div class="input-field">
+					<label for="firstname">First Name</label>
+					<input type="text" name="firstname" id="firstname" placeholder="First Name">
+					<label for="middlename">Middle Name</label>
+					<input type="text" name="middlename" id="middlename" placeholder="Middle Name">
+					<label for="lastname">Last Name</label>
+					<input type="text"  name="lastname" id="lastname" placeholder="Last Name">
+					<label for="contact">Mobile Number</label>
+					<input type="text"  name="contact" id="contact" placeholder="Phone Number">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" placeholder="Password">
+					<input type="submit" value="Sign Up" class="button" id="Sign_Up"/>
 
 				<div class="input-field">
 					<label for="firstname">First Name</label>
@@ -27,6 +48,7 @@ include("../resources/header.php");
 					<label for="password">Password</label>
 					<input type="password" name="password" placeholder="Password">
 					<input type="submit" value="Sign Up" class="button" id="Sign Up"/>
+
 				</div>
 			</form>
 
