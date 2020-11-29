61d63b77663e8127dc8f09c6a05b6bebdac86891
Ankush Khanna <ankushkhanna1998@gmail.com>
2020-11-29 06:17:49 +0530

    modifies error message

--- error/index.php
+++ error/index.php
@@ -3,7 +3,7 @@ include("../resources/header.php");
 ?>
 <center>
 	<img src="/epayments/resources/images/dog.jpg"/ height="20%" width="20%">
-	<h1>Oops! Something went wrong on our end.</h1>
+	<h1>Oops! Something went wrong!</h1>
 </center>
 <?php
 include("../resources/footer.php");
